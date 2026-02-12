fe77c949d4755d43a70ea85c6bebd2f68fb635ae
Luiz Felipe <vluizinho2@gmail.com>
2026-02-12 20:28:13 -0300

    subindo para conectar com o servidor

---
+++ src/main/java/com/controleestoque/controleestoque/ControleestoqueApplication.java
@@ -0,0 +1,13 @@
+package com.controleestoque.controleestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleestoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleestoqueApplication.class, args);
+	}
+
+}
